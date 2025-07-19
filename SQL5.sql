b7000365862c7dfeaf6b187e794a76422a3a9c81
Zehrakcin <96061496+Zehrakcin@users.noreply.github.com>
2025-07-19 14:36:22 +0300

    Add files via upload
    odev5

---
+++ SQL5.sql
@@ -0,0 +1,5 @@
+--1)SELECT * FROM film WHERE title ILIKE '%n' ORDER BY LENGTH(title) DESC LIMIT 5;
+
+--2)SELECT * FROM film WHERE title ILIKE '%n' ORDER BY LENGTH(title) ASC OFFSET 5 LIMIT 5;
+
+--3)SELECT * FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
